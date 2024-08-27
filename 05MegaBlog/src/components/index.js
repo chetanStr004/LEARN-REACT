0a8da157e28ffd598b303d3671fa8f0c060d35e4
chetanStr004 <chetansuthar9414@gmail.com>
2024-08-27 14:33:25 +0530

    vscode commit problem solved

---
+++ 05MegaBlog/src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/contain";
+import Logo from "./logo";
+import LogoutBtn from "./Header/logoutBtn";
+import Signup from "./SignUp";
+import Login from "./Login";
+import RTE from "./RTE";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    Input,
+    Select,
+    PostForm,
+    PostCard,
+    AuthLayout
+}
